49471958b268f87eebfe34f6086566caea8be0aa
katerina-5 <pochta13524@gmail.com>
2019-06-14 22:06:46 +0300

    Add not-found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomePageComponent } from './components/pages/welcome-page/welcome-page.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', component: WelcomePageComponent }
+  { path: '', component: WelcomePageComponent },
+  { path: 'not_found', component: NotFoundComponent },
+  { path: '**', redirectTo: 'not_found', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { MatCardModule } from '@angular/material/card';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { WelcomePageComponent } from './components/pages/welcome-page/welcome-page.component';
+import { NotFoundComponent } from './components/pages/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WelcomePageComponent
+    WelcomePageComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
