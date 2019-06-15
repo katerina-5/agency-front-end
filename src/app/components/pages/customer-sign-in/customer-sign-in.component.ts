6ece4de3ce90000e73b4f7b4f6472f37470f5583
katerina-5 <pochta13524@gmail.com>
2019-06-15 17:15:20 +0300

    Add function for authorization

--- src/app/components/pages/customer-sign-in/customer-sign-in.component.ts
+++ src/app/components/pages/customer-sign-in/customer-sign-in.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CustomerSignInComponent implements OnInit {
 
+  hide = true;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  auth(login, password) {
+    // TO DO:
+    // authorization request to server
   }
 
 }
